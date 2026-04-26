a4a094f3b0f67050d18c3b692ac6f4750c42359d
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2026-04-26 12:10:37 +0545

    updated validation loss calculation

--- projects/RUG -Tech/src/ai/postprocessing/metrics.py
+++ projects/RUG -Tech/src/ai/postprocessing/metrics.py
@@ -1,20 +1,28 @@
 import torch
 
-def evaluate(model, loader, criterion, device):
+def evaluate(model, val_loader, criterion, device,  mode="single"):
     model.eval()
-    total, correct, loss_sum = 0, 0, 0
+    correct = 0
+    total = 0
+    val_loss = 0
 
     with torch.no_grad():
-        for x, y in loader:
+        for x, y in val_loader:
             x, y = x.to(device), y.to(device)
 
             out = model(x)
             loss = criterion(out, y)
+            val_loss += loss.item()
+            if mode == "single":
+                _, preds = torch.max(out, 1)
+                correct += (preds == y).sum().item()
+                total += y.size(0)
+            else:
+                preds = torch.sigmoid(out) > 0.5
+                correct += (preds == y.bool()).sum().item()
+                total += y.numel()
 
-            loss_sum += loss.item()
-            _, preds = torch.max(out, 1)
+    avg_val_loss = val_loss / len(val_loader)
+    val_acc = correct / total
 
-            correct += (preds == y).sum().item()
-            total += y.size(0)
-
-    return loss_sum / len(loader), correct / total
+    return avg_val_loss, val_acc
